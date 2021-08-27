2f097ee8883107e55e5938d594cb8737ef1b8cb9
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-08-26 19:31:10 -0500

    initial commiut for oauth

---
+++ OAuth/main.go
@@ -0,0 +1,24 @@
+/*
+Oauth:
+User authorizes one website to do something at another website.
+- e.g. give a website access to your dropbox to store or retrieve files.
+- also use oauth to login.
+- it mainly just sends the general info about the user to the website.
+- can also give a website to post as you.
+
+Two most common ways to do oauth:
+Inflight credentials
+- more secure but requires server.
+Implicit
+- better for frontend, less secure.
+
+Oauth1
+- requries hmac signatures
+Oauth2
+- requires only https
+*/
+package main
+
+func main() {
+
+}
